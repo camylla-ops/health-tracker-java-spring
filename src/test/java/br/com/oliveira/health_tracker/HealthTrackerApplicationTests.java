8c32545e30c7d8c6b9dd8d0e9d0d68a948f5c6b7
camylla <camylla.oliveiraps@gmail.com>
2025-12-11 10:12:13 -0300

    feat: Initial project setup and environment configuration

---
+++ src/test/java/br/com/oliveira/health_tracker/HealthTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oliveira.health_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
